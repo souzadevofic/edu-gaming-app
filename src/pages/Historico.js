0d6611ef1881848eb0c44bcc46b14d3758a4360e
Matheus <matheussa2@hotmail.com>
2025-04-27 15:47:26 -0300

    Alt teste Matheus

--- src/pages/Historico.js
+++ src/pages/Historico.js
@@ -5,7 +5,7 @@ import { View, Text } from 'react-native';
 export default function Historico() {
   return (
     <View>
-      <Text>Histórico</Text>
+      <Text>Histórico !!!!</Text>
     </View>
   );
 }
