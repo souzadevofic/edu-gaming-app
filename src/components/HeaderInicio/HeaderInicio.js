b00b9c196b17e6e1adb1c047d1ec4091a350791c
WebersonCosta <weberson.dev0@gmail.com>
2025-04-30 14:29:21 -0300

    Primeira modificação do HeaderInicio.js

--- src/components/HeaderInicio/HeaderInicio.js
+++ src/components/HeaderInicio/HeaderInicio.js
@@ -1,23 +1,42 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function HeaderInicio(){
+export default function HeaderInicio({ nomeUsuario, matricula, nomeCurso }) {
 
     const styles = StyleSheet.create({
         container_header_inicio: {
             backgroundColor: "#2aacc0",
             height: 250,
-            width: "100%"
+            width: "100%",
+            justifyContent: 'flex-end',
+            padding: 20,
+        },
+        titleName: {
+            color: "#fff",
+            fontSize: 28,
+            fontWeight: "bold",
+            marginBlock: 8,
+        },
+        textMat: {
+            color: "#fff",
+            fontSize: 16,
+            fontWeight: "300",
+            marginBottom: 4,
+        },
+        textCourse: {
+            color: "#fff",
+            fontSize: 20,
+            fontWeight: "600",
         }
     });
 
-    return(
+    return (
         <View style={styles.container_header_inicio}>
-            {/* <View>
-                <Text>Olá, Usuário</Text>
-                <Text>Nome do curso:</Text>
-                <Text>Matricula:</Text>
-            </View> */}
+            <View>
+                <Text style={styles.titleName}>Olá, {nomeUsuario}</Text>
+                <Text style={styles.textMat}>{matricula}</Text>
+                <Text style={styles.textCourse}>{nomeCurso}</Text>
+            </View> 
         </View>
     );
 }
--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -4,6 +4,9 @@ import HeaderInicio from '../components/HeaderInicio/HeaderInicio';
 // Os componentes serão importados aqui e retonados dentro do return ex: <HeaderInicio/>
 export default function Inicio() {
   return (
-    <HeaderInicio/>
+    <HeaderInicio   
+      nomeUsuario="Maria Silva" 
+      matricula="123456789" 
+      nomeCurso="Análise e Desenvolvimento de Sistemas" />
   );
 }
